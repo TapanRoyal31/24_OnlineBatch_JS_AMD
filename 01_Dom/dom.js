9f329551918472520b461e99f422d194f7eac743
TapanRoyal31 <tapanr3105@gmail.com>
2025-11-11 09:15:05 +0530

    "Dom selectors"

---
+++ 01_Dom/dom.js
@@ -0,0 +1,22 @@
+// 1) getelementbyclassname 
+// 2) getelemetnbyid
+// 3) queryselector
+// 4) queryselctorall
+
+
+// getelementbyclassname
+// let class1 = document.getElementsByClassName("myclass");
+
+
+let id1 = document.getElementById("myid");
+
+
+let query = document.querySelector("p");
+
+
+// let all = document.querySelectorAll("p")
+
+// console.log("query selector")
+// console.log(query);
+// console.log("query selectorall")
+// console.log(all);
