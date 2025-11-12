65c8be702776fd11e7f04fefdf83add83735c2d2
TapanRoyal31 <tapanr3105@gmail.com>
2025-11-12 18:39:43 +0530

    "Practice task"

---
+++ 01_Dom/Task/task.js
@@ -0,0 +1,12 @@
+/*
+Task 1: Update the main title’s text.
+Task 2: Change paragraph content using innerHTML.
+Task 3: Get and display the innerText of a list item.
+Task 4: Add a new element using innerHTML.
+Task 5: Display the subtitle text below the button using innerText.
+Task 6: Replace the entire list using innerHTML.
+Task 7: Add a new <div> inside the content section using innerHTML.
+Task 8: Get a paragraph’s text and display it using an alert box.
+Task 9: Append a new <li> item to the existing list using innerHTML.
+Task 10: Update the main heading using textContent.
+*/
